b7b5390fa37f065beb36125f423c6438e5e270bd
Shuaipeng Yu <jackysp@gmail.com>
2020-01-11 14:21:24 +0800

    change
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
 
 	"github.com/gohugoio/hugo/commands"
 )
 
 func main() {
+	os.RemoveAll("themes/kiss")
+	commandLine("git", []string{"clone", "https://github.com/ribice/kiss.git", "themes/kiss"})
 	resp := commands.Execute([]string{})
 	if resp.Err != nil {
 		log.Fatal(resp.Err)
+	}
+}
+
+func commandLine(name string, args []string) {
+	log.Println(name, args)
+	out, err := exec.Command(name, args...).Output()
+	if err != nil {
+		if len(out) != 0 {
+			log.Print(string(out))
+		}
+		log.Fatal(err)
+	}
+	if len(out) != 0 {
+		log.Print(string(out))
 	}
 }
